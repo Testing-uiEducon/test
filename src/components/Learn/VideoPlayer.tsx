338290aae526d022d3fd02f615ce812c75c37c0e
Sandeep Jha <91358961+ft-jsandeep@users.noreply.github.com>
2024-06-25 01:06:39 +0530

    player ended functionality added

--- src/components/Learn/VideoPlayer.tsx
+++ src/components/Learn/VideoPlayer.tsx
@@ -2,8 +2,6 @@ import { LearnState } from "@/context/LearnContextProvider";
 import { handleGet } from "@/core/api-calls/Axios";
 import React, { useEffect, useRef, useState } from "react";
 import ReactPlayer from "react-player";
-import axios from "axios";
-import Image from "next/image";
 import CourseCompletion from "./CourseCompletion";
 
 type Props = {
@@ -14,8 +12,7 @@ type Props = {
 const VideoPlayer = ({ onEndedHandler, thumbnail }: Props) => {
   const { currentContent } = LearnState();
   const [playState, setPlayState] = useState<string | undefined>("loading");
-
-  const iframeRef = useRef(null);
+  const [scriptLoaded, setScriptLoaded] = useState<boolean>(false);
 
   const fetchPlayableLink = async () => {
     try {
@@ -34,13 +31,91 @@ const VideoPlayer = ({ onEndedHandler, thumbnail }: Props) => {
     fetchPlayableLink();
   }, [currentContent]);
 
+  useEffect(() => {
+    console.log("Player component mounted");
+
+    const loadScript = () => {
+      return new Promise<void>((resolve, reject) => {
+        if (scriptLoaded) resolve();
+        else {
+          const script = document.createElement("script");
+          script.src = "https://static.testpress.in/static/js/player.js";
+          script.async = true;
+          script.onload = () => resolve();
+          script.onerror = () => reject(new Error("Failed to load script"));
+          document.body.appendChild(script);
+        }
+      });
+    };
+
+    loadScript()
+      .then(() => {
+        setScriptLoaded(true);
+        const iframe = document.querySelector(
+          "#iframe_player"
+        ) as HTMLIFrameElement;
+        if (iframe) {
+          console.log("Player log: connected..");
+
+          const player = new (window as any).Testpress.Player(iframe);
+          // const getTotalWatchTime = () => {
+          //   if (isPlaying) {
+          //     player
+          //       .played()
+          //       .then((ranges: [number, number][]) => {
+          //         let totalWatchTime = 0;
+          //         ranges.forEach((range: [number, number]) => {
+          //           totalWatchTime += range[1] - range[0];
+          //         });
+          //         console.log(
+          //           "Total watch time:",
+          //           totalWatchTime.toFixed(2),
+          //           "seconds"
+          //         );
+          //       })
+          //       .catch((error: any) => {
+          //         console.error("Error getting played ranges:", error);
+          //       });
+          //   }
+          // };
+
+          // player.on("play", () => {
+          //   isPlaying = true;
+          // });
+
+          // player.on("pause", () => {
+          //   isPlaying = false;
+          // });
+
+          player.on("ended", () => {
+            console.log("ended");
+            onEndedHandler();
+          });
+        } else {
+          console.log("no iframe");
+        }
+      })
+      .catch((error) => {
+        console.error(error.message);
+      });
+
+    return () => {
+      const script = document.querySelector(
+        'script[src="https://static.testpress.in/static/js/player.js"]'
+      );
+      if (script) {
+        document.body.removeChild(script);
+      }
+    };
+  }, [playState]);
+
   return (
     <div className="w-full h-fit lg:w-[95%] lg:h-[95%] border border-solid border-[#e1e1e1] bg-[#212121] flex items-center justify-center">
       {/* <ReactPlayer
         url={
           playState == "loading"
             ? "https://europe1.discourse-cdn.com/figma/original/3X/6/1/6173a1aae8372541cfe858bbadccf198e2935f89.gif"
-            : playState
+            : "https://d28qihy7z761lk.cloudfront.net/transcoded/AKTdMZJkTT2/video.m3u8"
         }
         config={{
           file: {
@@ -67,9 +142,9 @@ const VideoPlayer = ({ onEndedHandler, thumbnail }: Props) => {
             className="w-full h-full"
             src={playState}
             allowFullScreen
-            ref={iframeRef}
+            id="iframe_player"
           ></iframe>
-          <button
+          {/* <button
             type="button"
             className={`inline-flex items-center px-4 py-2 bg-[#7e3af2] text-white font-medium rounded-lg rounded-r-none hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 absolute top-0 end-0`}
             onClick={onEndedHandler}
@@ -89,7 +164,7 @@ const VideoPlayer = ({ onEndedHandler, thumbnail }: Props) => {
                 d="M9 5l7 7-7 7"
               />
             </svg>
-          </button>
+          </button> */}
         </div>
       )}
     </div>
--- src/pages/courses/[courseId]/learn.tsx
+++ src/pages/courses/[courseId]/learn.tsx
@@ -2,11 +2,20 @@ import LearnContainer from "@/components/Learn/LearnContainer";
 import EmailVerifyWrapper from "@/components/wrappers/email-verify-wrapper";
 import PrivateRouteWrapper from "@/components/wrappers/private-route-wrapper";
 import LearnContextProvider from "@/context/LearnContextProvider";
+import Head from "next/head";
+import Script from "next/script";
 import React from "react";
 
 type Props = {};
 
 const learn = (props: Props) => {
+  <Head>
+    <script src="https://static.testpress.in/static/js/player.js"></script>
+    <script>
+      var iframe = document.querySelector('iframe'); var player = new
+      Testpress.Player(iframe); console.log(player);
+    </script>
+  </Head>;
   return (
     <PrivateRouteWrapper>
       <EmailVerifyWrapper>
