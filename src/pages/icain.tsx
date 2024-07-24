cf6e85d14233f27d4f8d39cc7b3055ea4abe515a
Paras Jain <64364245+ParasJain013@users.noreply.github.com>
2024-07-24 23:32:14 +0530

    fix

--- src/pages/icain.tsx
+++ src/pages/icain.tsx
@@ -7,23 +7,8 @@ const Icain = () => {
   const { destination } = router.query;
 
   useEffect(() => {
-    if (destination) {
-      // Define your redirections here
-      const redirects: { [key: string]: string } = {
-        'icain': 'https://www.icain-conf.com',
-        'icdpn': 'https://www.icdpn-conf.com',
-        // Add more as needed
-      };
-
-      const targetUrl = redirects[destination as string];
-
-      if (targetUrl) {
-        window.location.href = targetUrl;
-      } else {
-        router.push('/');
-      }
-    }
-  }, [destination, router]);
+        window.location.href = 'https://www.icain-conf.com';
+  }, []);
 
   return <p>Redirecting...</p>;
 };
